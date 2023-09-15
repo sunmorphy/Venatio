b72ca32e46d8d58bb8a12ea1f8cc8524badda597
Andikas <andpuji27@gmail.com>
2023-09-15 11:47:32 +0800

    Initial Commit

---
+++ Venatio/VenatioApp.swift
@@ -0,0 +1,17 @@
+//
+//  VenatioApp.swift
+//  Venatio
+//
+//  Created by user236913 on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct VenatioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
